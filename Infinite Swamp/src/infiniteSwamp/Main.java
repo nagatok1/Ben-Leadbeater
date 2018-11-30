2abf640e16a49e684470b1114258d0fa9d0f7b33
Ben Leadbeater <40888832+nagatok1@users.noreply.github.com>
2018-11-30 16:09:02 +0000

    Add files via upload

--- Infinite Swamp/src/infiniteSwamp/Main.java
+++ Infinite Swamp/src/infiniteSwamp/Main.java
@@ -7,3 +7,9 @@ public class Main {
 		Menu.Menu1();
 	}
 }
+
+
+// Can Move on an X Y axis
+// Terrain to be implemented
+// Find Treasure to end game
+// Visible Treasure Location in Menu for Testing Purposes would be removed for actual game
